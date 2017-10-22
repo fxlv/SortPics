bee1c44478ddaf66d358aab6ed12814058112813
Kaspars Mickēvičs <kaspars@fx.lv>
2017-10-22 17:04:25 +0300

    Moved out Image class to separate file

---
+++ SortPics/Image.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortPics
+{
+    /// <summary>
+    /// Image object
+    /// </summary>
+    class Image
+    {
+
+        public string FilePath { set; get; }
+        public DateTime ModificationDate { set; get; }
+        public DateTime CreationDate { set; get; }
+        public string FileName { set; get; }
+
+        public Image(string path, DateTime modificationDate, DateTime creationDate)
+        {
+            FilePath = path;
+            FileName = Path.GetFileName(FilePath);
+            ModificationDate = modificationDate;
+            CreationDate = creationDate;
+        }
+
+        public override string ToString()
+        {
+            string stringRepresentation = $"Image: {FileName}";
+            return stringRepresentation;
+        }
+    }
+}
--- SortPics/Program.cs
+++ SortPics/Program.cs
@@ -60,30 +60,7 @@ namespace SortPics
 
         }
     }
-    /// <summary>
-    /// Image object
-    /// </summary>
-    class Image
-    {
 
-        public string FilePath { set; get; }
-        public DateTime ModificationDate { set; get; }
-        public DateTime CreationDate { set; get; }
-        public string FileName { set; get;  }
-
-        public Image(string path, DateTime modificationDate, DateTime creationDate) {
-            FilePath = path;
-            FileName = Path.GetFileName(FilePath);
-            ModificationDate = modificationDate;
-            CreationDate = creationDate;
-        } 
-
-        public override string ToString()
-        {
-            string stringRepresentation = $"Image: {FileName}";
-            return stringRepresentation;
-        }
-    }
     class Program
     {
 
