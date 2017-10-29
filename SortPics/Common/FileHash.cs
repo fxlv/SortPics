1a0acd0c8054a2e4fe388257ec4fb813f57f1b31
Kaspars Mickēvičs <kaspars@fx.lv>
2017-10-29 16:16:51 +0200

    Added FileHash.cs

---
+++ SortPics/Common/FileHash.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortPics.Common
+{
+    class FileHash
+    {
+        public static string GetMd5Hash(string fileName)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(fileName))
+                {
+                    return BitConverter.ToString(md5.ComputeHash(stream));
+                }
+            }
+
+        }
+
+    }
+}
