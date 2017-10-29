9de6a6390a0ca49366f2a47d9ca2663563ca24f2
Kaspars Mickēvičs <kaspars@fx.lv>
2017-10-29 15:47:52 +0200

    Added UserInput.cs

---
+++ SortPics/Common/UserInput.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+namespace SortPics.Common
+{
+    internal class UserInput
+    {
+        public static bool ConfirmContinue(string msg = null)
+        {
+            while (true)
+            {
+                if (msg != null)
+                    WriteLine(msg);
+                Write("Please answer with y/n: ");
+                var userResponse = ReadLine();
+                switch (userResponse)
+                {
+                    case "y":
+                        return true;
+                    case "n":
+                        return false;
+                    default:
+                        continue;
+                }
+            }
+        }
+    }
+}
