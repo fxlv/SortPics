0f0e1418b92af514a41b71e9c19117c0e1a24554
Kaspars Mickēvičs <kaspars@fx.lv>
2017-11-05 12:37:35 +0200

    Removing references to mstest

--- Tests/MediaFilesTest.cs
+++ Tests/MediaFilesTest.cs
@@ -8,15 +8,15 @@ using SortPics.Images;
 [TestFixture]
 public class MediaFilesTest
 {
-    private string imagesDirectory;
-    private List<MediaFile> images;
-
     [SetUp]
     public void SetUpImages()
     {
         imagesDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "testfiles");
         images = Images.FindImages(imagesDirectory);
     }
+
+    private string imagesDirectory;
+    private List<MediaFile> images;
 
 
     [Test]
@@ -26,70 +26,57 @@ public class MediaFilesTest
     }
 
     [Test]
-    public void TestPngFile()
-    {
-        var testImagePng = images.Where(s => s.FileName == "computer-2893112_640.png").FirstOrDefault();
-        Assert.IsTrue(testImagePng.IsImage);
-        Assert.AreEqual(testImagePng.MimeType, "image/png");
-
-    }
-    [Test]
-    public void TestJpgFile()
-    {
-        var testImagePng = images.Where(s => s.FileName == "nobody-2798850_640.jpg").FirstOrDefault();
-        Assert.IsTrue(testImagePng.IsImage);
-        Assert.AreEqual(testImagePng.MimeType, "image/jpeg");
-
-    }
-
-    [Test]
     public void TestDetectIphone()
     {
         var testImagePng = images.Where(s => s.FileName == "20170126_012712498_iOS.jpg").FirstOrDefault();
         Assert.IsTrue(testImagePng.IsImage);
         Assert.AreEqual(testImagePng.MimeType, "image/jpeg");
         Assert.AreEqual(testImagePng.CameraMake, "Apple");
-        Assert.AreEqual(testImagePng.CameraModel,"iPhone 6s");
+        Assert.AreEqual(testImagePng.CameraModel, "iPhone 6s");
     }
+
+    [Test]
+    public void TestFilteringByYear()
+    {
+        var imagesFiltered = Images.FilterImages(images, 2017, 0, 0);
+        Assert.AreEqual(5, imagesFiltered.Count);
+    }
+
+    [Test]
+    public void TestFilteringByYearAndMonth()
+    {
+        var imagesFiltered = Images.FilterImages(images, 2017, 2, 0);
+        Assert.AreEqual(2, imagesFiltered.Count);
+    }
+
+    [Test]
+    public void TestFilteringByYearAndMonthShouldBeOne()
+    {
+        var imagesFiltered = Images.FilterImages(images, 2017, 1, 0);
+        Assert.AreEqual(1, imagesFiltered.Count);
+    }
+
+    [Test]
+    public void TestFilteringByYearAndMonthShouldBeZero()
+    {
+        var imagesFiltered = Images.FilterImages(images, 2017, 5, 0);
+        Assert.AreEqual(0, imagesFiltered.Count);
+    }
+
     [Test]
     public void TestGeoTagging()
     {
         var testImagePng = images.Where(s => s.FileName == "20170126_012712498_iOS.jpg").FirstOrDefault();
         Assert.AreEqual(testImagePng.GpsLatitude, "37° 49' 41.88\"");
         Assert.AreEqual(testImagePng.GpsLongitude, "-122° 29' 51.49\"");
-
     }
 
     [Test]
-    public void TestFilteringByYear()
+    public void TestJpgFile()
     {
-        var imagesFiltered = SortPics.Images.Images.FilterImages(images, 2017, 0, 0);
-        Assert.AreEqual(5, imagesFiltered.Count);
-
-    }
-
-    [Test]
-    public void TestFilteringByYearAndMonthShouldBeOne()
-    {
-        var imagesFiltered = SortPics.Images.Images.FilterImages(images, 2017, 1, 0);
-        Assert.AreEqual(1, imagesFiltered.Count);
-
-    }
-
-    [Test]
-    public void TestFilteringByYearAndMonthShouldBeZero()
-    {
-        var imagesFiltered = SortPics.Images.Images.FilterImages(images, 2017, 5, 0);
-        Assert.AreEqual(0, imagesFiltered.Count);
-
-    }
-
-    [Test]
-    public void TestFilteringByYearAndMonth()
-    {
-        var imagesFiltered = SortPics.Images.Images.FilterImages(images, 2017, 2, 0);
-        Assert.AreEqual(2, imagesFiltered.Count);
-
+        var testImagePng = images.Where(s => s.FileName == "nobody-2798850_640.jpg").FirstOrDefault();
+        Assert.IsTrue(testImagePng.IsImage);
+        Assert.AreEqual(testImagePng.MimeType, "image/jpeg");
     }
 
     [Test]
@@ -97,5 +84,13 @@ public class MediaFilesTest
     {
         var testImagePng = images.Where(s => s.FileName == "computer-2893112_640.png").FirstOrDefault();
         Assert.AreEqual(testImagePng.ToString(), "Media file: computer-2893112_640.png");
+    }
+
+    [Test]
+    public void TestPngFile()
+    {
+        var testImagePng = images.Where(s => s.FileName == "computer-2893112_640.png").FirstOrDefault();
+        Assert.IsTrue(testImagePng.IsImage);
+        Assert.AreEqual(testImagePng.MimeType, "image/png");
     }
 }
